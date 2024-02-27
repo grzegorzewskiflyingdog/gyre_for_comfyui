8046662b68023030e3a3892293e716e93dfc3dcd
Marcin Grzegorzewski <grzegorzewski@flyingdog.de>
2024-02-27 15:14:59 +0100

    fix

--- __init__.py
+++ __init__.py
@@ -26,10 +26,30 @@ comfy_path = os.path.dirname(folder_paths.__file__)
 db_dir_path = os.path.join(workspace_path, "db")
 
 workspace_app = web.Application()
-dist_path = os.path.join(workspace_path, 'dist/bundle')
+dist_path = os.path.join(workspace_path, 'dist/build')
 if os.path.exists(dist_path):
     workspace_app.add_routes([
         web.static("/", dist_path),
     ])
 
-server.PromptServer.instance.app.add_subapp("/workspace_web/", workspace_app)
+server.PromptServer.instance.app.add_subapp("/build/", workspace_app)
+
+
+
+@server.PromptServer.instance.routes.post("/workspace/update_file")
+async def update_file(request):
+    data = await request.json()
+    file_path = data['file_path']
+    json_str = data['json_str']
+
+    def write_json_to_file(json_str):
+        my_workflows_dir = get_my_workflows_dir()
+        full_path = os.path.join(my_workflows_dir, file_path)
+        # Create the directory if it doesn't exist
+        os.makedirs(os.path.dirname(full_path), exist_ok=True)
+        with open(full_path, 'w', encoding='utf-8') as file:
+            file.write(json_str)
+
+    # Offload the file update to a separate thread
+    await asyncio.to_thread(write_json_to_file, json_str)
+    return web.Response(text="File updated successfully")
